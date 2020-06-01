c21e64a9934a666ec63ae669898b259527f9d730
Marcos Silva <tio.makin@gmail.com>
2020-06-01 15:46:08 +0300

    fix(): bug on splice method

--- app/users/daos/in.memory.dao.ts
+++ app/users/daos/in.memory.dao.ts
@@ -68,7 +68,7 @@ export class GenericInMemoryDao {
 
     removeUserById(userId: string) {
         const objIndex = this.users.findIndex((obj: { id: any; }) => obj.id === userId);
-        this.users = this.users.splice(objIndex, objIndex);
+        this.users = this.users.splice(objIndex, 1);
         return new Promise((resolve) => {
             resolve(`${userId} removed`);
         });
